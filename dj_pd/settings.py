946b682497fcddace7296d60be8ea4aff3c8a8dd
lim8540 <lim8540@naver.com>
2021-01-06 12:11:32 +0900

    static setting

--- dj_pd/settings.py
+++ dj_pd/settings.py
@@ -130,7 +130,3 @@ STATIC_ROOT = os.path.join(os.path.dirname(
     BASE_DIR), "static_cdn", "static_root"
 )
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(
-    BASE_DIR), "static_cdn", "media_root"
-)
